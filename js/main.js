85a51491918591a91d9f36a892886373662a432b
omurkullove <omurkulovaidin94@gmail.com>
2022-08-11 19:59:46 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,91 @@
+// let arrive = prompt("When did you come?");
+// if (arrive >= 10 && arrive <= 22) {
+//   alert("Office is open, Welcome!");
+// } else {
+//   alert("Go away, We closed!");
+// }
+
+//TODO:-------------------------<Данные из БД>-------------------
+// const eamil = prompt("Add email");
+// const password = prompt("Add password");
+// if (email === "user123" && password === "ps123") {
+//   alert("YOU ARE WELCOME!, user123");
+// } else {
+//   alert("Incorrect!");
+// }
+
+// const email = prompt("Add email");
+// const password = prompt("Add password");
+// // console.log(email, password);
+// // console.log(Boolean(email));
+// if (email === null || password === null || email === "" || password === "") {
+//   alert("Blunck the sheet!");
+// } else if (email === "user123" && password === "ps123") {
+//   alert("YOU ARE WELCOME!, user123");
+// } else {
+//   alert("SAVED!");
+// }
+
+// let password = prompt("Enter password");
+// let tries = 1;
+// if (password === "ps123") {
+//   alert("password is correct");
+// } else {
+//   password = prompt("Try again!");
+//   console.log(password);
+//   if (password === "ps123") {
+//     alert("password is correct");
+//   } else {
+//     alert("Your account has been blocked");
+//   }
+// }
+
+// let email = prompt("Add email");
+// let tries = 1;
+// if (email === "user123") {
+//   alert("Email is correct!");
+// } else {
+//   prompt("Try one more time");
+//   console.log(email);
+//   if (email === "user123") {
+//     alert("Email is correct!");
+//   } else {
+//     alert("Your account hase been blocked!");
+//   }
+// }
+// let answer = confirm("You want to leave game?");
+// console.log(answer);
+// if (answer) {
+//   alert("Goodbye Mutherfucker!");
+// } else {
+//   alert("Lets play, baby!");
+// }
+
+// //! ИНКРЕМЕНТ->УВЕЛ. НА 1 (++)   //  ДEКРЕМЕНТ (--)
+// let likes = 5;
+// let answer = confirm("Do you want to like?");
+// if (answer) {
+//   likes++;
+//   console.log(likes);
+//   alert(`You liked it , total: ${likes}`);
+// }
+// confirm("Do you want to like?");
+// if (answer) {
+//   likes++;
+//   console.log(likes);
+//   alert(`You liked it , total: ${likes}`);
+// }
+// let classroom = confirm("Did you do tasks from classroom?");
+// let platform = confirm("Did you do tasks from platform?");
+// if (classroom && platform) {
+//   alert("Well done! Good job! Amazing!");
+// } else {
+//   alert("Practice makes perfect!");
+// }
+
+// let homework = prompt("Did you do <momentum?>");
+// if (homework === "momentum" || homework === "teamflow") {
+//   alert("100 points");
+// } else {
+//   alert("0 points");
+// }
